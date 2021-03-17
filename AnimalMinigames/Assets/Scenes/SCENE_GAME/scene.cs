070e01527adfaf824d1bea4fae923f862519e732
ramGthunderRed <80571988+ramGthunderRed@users.noreply.github.com>
2021-03-17 11:35:52 +0900

    scene change
    test

---
+++ AnimalMinigames/Assets/Scenes/SCENE_GAME/scene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour
+{
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("StageScene");
+    }
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("GameScene_001");
+    }
+}
