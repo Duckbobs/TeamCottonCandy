4c68ce0b0bd68b85c45121ecb64bdbd9f6709081
Duckbobs <45709904+Duckbobs@users.noreply.github.com>
2021-03-22 01:19:23 +0900

    Revert "Revert "Merge branch '미니게임-추가' into main""
    This reverts commit 594c46c07dc4d4dbd198f450bc9634cca6afc0bd.

---
+++ AnimalMinigames/Assets/UI_BtnDownEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_BtnDownEvent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool isBtnDown = false;
+
+    private void Update()
+    {
+        if (isBtnDown)
+        {
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isBtnDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isBtnDown = false;
+    }
+
+}
