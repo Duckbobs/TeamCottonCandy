dc3e0a2551704993cc0224f7b708349887599bf1
wotjd7359 <wotjd7359@hanmail.net>
2021-03-21 16:12:59 +0900

    (Added) Cinemachine package, Stage select scene.

---
+++ AnimalMinigames/Assets/Scenes/SCENE_STAGE_SELECT/StageSelectorEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectorEvent : MonoBehaviour
+{
+    public GameObject StagePanel;
+
+    private void Awake()
+    {
+        StagePanel.SetActive(false);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        StagePanel.SetActive(true);
+    }
+}
---
+++ AnimalMinigames/Assets/Scenes/SCENE_STAGE_SELECT/StageSelectorScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectorScroll : MonoBehaviour
+{
+    public Transform CameraTransform;
+    private Vector3 MousePosition;
+
+    private void OnMouseDown()
+    {
+        MousePosition = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 distance = Input.mousePosition - MousePosition;
+        MousePosition = Input.mousePosition;
+        CameraTransform.Translate(new Vector3(0.0f, -distance.y, 0.0f) * Time.deltaTime);
+    }
+}
