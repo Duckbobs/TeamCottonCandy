ab7094e033c84b3779c66bc6354461ec21ba0b62
Duckbob <skadlf011@naver.com>
2021-04-09 01:31:26 +0900

    변수보안, 네트워크, 스테이지 꾸미기

---
+++ AnimalMinigames/Assets/Scripts/Secure/GlobalValues.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalValues
+{
+    public static void init()
+    {
+        Global.Set("mineral", 0);
+    }
+}
---
+++ AnimalMinigames/Assets/Scripts/Secure/UITextAutoSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextAutoSet : MonoBehaviour
+{
+    Text uiText;
+    public string key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = Global.Get(key).ToString();
+    }
+}
