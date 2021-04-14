c5b764ff90ea610b7f406a6e57118127c78acef3
Duckbob <skadlf011@naver.com>
2021-04-14 11:41:31 +0900

    메인화면

---
+++ AnimalMinigames/Assets/Scenes/SCENE_START/GameStarter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+    public string sceneName;
+    private void OnMouseDown()
+    {
+        SceneChanger.Load_Async(sceneName);
+    }
+}
---
+++ AnimalMinigames/Assets/Scenes/SCENE_START/TextAlpha.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAlpha : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Text compText;
+    float alpha = 1.0f;
+    float sign = -1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(alpha <= 0.7f)
+            sign = 1.0f;
+        else if (alpha >= 1.0f)
+            sign = -1.0f;
+
+        alpha += sign * speed * Time.deltaTime;
+
+        compText.color = new Color(
+            compText.color.r
+            , compText.color.g
+            , compText.color.b
+            , alpha);
+    }
+}
