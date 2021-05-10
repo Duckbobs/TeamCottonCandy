a657a7951c713142ab05de277d2496919fc10a8a
ramGthunderRed <80571988+ramGthunderRed@users.noreply.github.com>
2021-05-11 00:12:16 +0900

    mouse effect - heart

---
+++ AnimalMinigames/Assets/Scenes/touch_effect/MouseEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEffect : MonoBehaviour
+{
+    public GameObject Dokkidokki_effect;
+    float spawnsTime;
+    public float defaultTime = 0.05f;
+    void Update()
+    {
+        if (Input.GetMouseButton(0)&&spawnsTime>=defaultTime)
+        {
+            HeartCreat();
+            spawnsTime = 0;
+        }
+        spawnsTime += Time.deltaTime;
+    }
+
+    void HeartCreat()
+    {
+        Vector3 mPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mPosition.z = 0;
+        Instantiate(Dokkidokki_effect, mPosition, Quaternion.identity);
+    }
+}
