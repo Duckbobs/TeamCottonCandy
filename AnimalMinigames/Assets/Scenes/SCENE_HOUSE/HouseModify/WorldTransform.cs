858b4fcc84a0f3673fec438c2e5eb423fd35e493
wotjd834 <wotjd834@hanmail.net>
2021-05-22 15:04:49 +0900

    Add HouseModify

---
+++ AnimalMinigames/Assets/Scenes/SCENE_HOUSE/HouseModify/WorldTransform.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WorldTransform : MonoBehaviour, IDragHandler
+{
+    public float zoomSpeed = 1.0f;
+    public float maxZoom = 2.0f;
+    public float minZoom = 1.0f;
+    private RectTransform rectTransform;
+    private Vector3 prevMousePosition;
+    private bool mouseButtonPressed = false;
+    public float extent = 0;
+   
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zoom();
+        autoTranslate();
+        commonVariable.worldTransform = rectTransform.anchoredPosition;
+        //Debug.Log(commonVariable.worldTransform);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Wow");
+        var position = rectTransform.anchoredPosition;
+        position.x += eventData.delta.x;
+        //position += eventData.delta;
+        rectTransform.anchoredPosition = position;
+    }
+
+    void zoom()
+    {
+        float zoomDifference = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        var scale = rectTransform.localScale;
+        if (zoomDifference < 0)
+        {
+            commonVariable.worldScaleCoefficient = Mathf.Max(minZoom, commonVariable.worldScaleCoefficient + zoomDifference);
+        }
+        else
+        {
+            commonVariable.worldScaleCoefficient = Mathf.Min(maxZoom, commonVariable.worldScaleCoefficient + zoomDifference);
+        }
+        scale.x = scale.y = commonVariable.worldScaleCoefficient;
+        rectTransform.localScale = scale;
+        extent = 300 * (commonVariable.worldScaleCoefficient - 1);
+    }
+
+    void autoTranslate()
+    {
+        var position = rectTransform.anchoredPosition;
+        position.x = Mathf.Max(-extent, Mathf.Min(extent, position.x));
+        rectTransform.anchoredPosition = position;
+    }
+
+}
---
+++ AnimalMinigames/Assets/Scenes/SCENE_HOUSE/HouseModify/commonVariable.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class commonVariable : MonoBehaviour
+{
+    public int countOfSprites = 64;
+    public Sprite[] _spriteData;
+    public int countOfInventoryMenu = 4;
+    public GameObject[] _inventoryMenu;
+    public GameObject _worldItem;
+
+    static public Vector2 worldTransform;
+    static public float worldScaleCoefficient = 1.0f;
+
+    static public int onGrid = 0;
+    static public int gridX;
+    static public int gridY;
+
+    static public bool onTrashcan;
+    
+    static public GameObject tempItem;
+    static public Sprite[] spriteData;
+    static public int[] slotData;
+    static public int currentItemCode;
+    static public int currentTheme;
+    static public GameObject[] inventoryMenu;
+    static public GameObject worldItem;
+
+    void Awake()
+    {
+        spriteData = new Sprite[countOfSprites];
+        for( int i = 0; i != countOfSprites; i++)
+        {
+            spriteData[i] = _spriteData[i];
+        }
+        inventoryMenu = new GameObject[countOfInventoryMenu];
+        for( int i = 0; i != countOfInventoryMenu; i++)
+        {
+            inventoryMenu[i] = _inventoryMenu[i];
+        }
+        worldItem = _worldItem;
+        slotData = new int[20];
+        for( int i = 0; i != 20; i++ )
+        {
+            slotData[i] = -1;
+        }
+    }
+
+    
+    static public void PutWorldItem(int y, int x, int ItemCode)
+    {
+        slotData[y * 10 + x] = ItemCode;
+        if (ItemCode == -1) return;
+        GameObject world = GameObject.Find("World");
+        var focusedItem = GameObject.Instantiate(worldItem, world.transform);
+        var image = focusedItem.GetComponent<Image>();
+        image.sprite = spriteData[ItemCode];
+        var rectTransform = focusedItem.GetComponent<RectTransform>();
+        var pos = rectTransform.anchoredPosition;
+        pos.y = -60 * y - 30;
+        pos.x = 60 * x + 30;
+        rectTransform.anchoredPosition = pos;
+    }
+
+    static public bool AvailableToPut(int y, int x)
+    {
+        return slotData[y * 10 + x] == -1;
+    }
+
+    static public int GetItemCode(int y, int x)
+    {
+        return slotData[y * 10 + x];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
